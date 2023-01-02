617721034454b4b7dc5d26bf22049c444fc680f1
samuel hughes <samuelhugh33@gmail.com>
2023-01-01 21:39:47 -0600

    Refactoring

--- Fundamentals/coding_along/coding_along.py
+++ Fundamentals/coding_along/coding_along.py
@@ -1,2 +1,4 @@
-favorite_color = input('What is your favorite color? ') # input takes a prompt, which needs to be a string
-print(f'Your favorite color is: {favorite_color}') #output, prints the color given to the console
+# Input takes a prompt, which needs to be a String
+favorite_color = input('What is your favorite color? ')
+# Output, prints the color given to the console, Also using F-String
+print(f'Your favorite color is: {favorite_color}')
