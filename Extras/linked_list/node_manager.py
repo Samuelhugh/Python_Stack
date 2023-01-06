503e217c80ae3639456a3ff84e01dde298a9bd76
samuel hughes <samuelhugh33@gmail.com>
2023-01-06 00:10:42 -0600

    Practicing Data Structures

--- Extras/linked_list/node_manager.py
+++ Extras/linked_list/node_manager.py
@@ -1,6 +1,15 @@
+from node import SLNode
+
 class SList:
     def __init__(self):
         self.head = None
+# add_to_front Method
+    def add_to_front(self, value):
+        new_node = SLNode(value)
+# Saving current head into variable, so I don't lose access to it
+        current_head = self.head
+# This line of code is so my new New Head has access to the previous current_head
+        new_node.next = current_head
 
 
 
