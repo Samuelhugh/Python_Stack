e8397e617880e4fcc525a31f15e226bfa5d5e644
samuel hughes <samuelhugh33@gmail.com>
2023-01-05 23:28:36 -0600

    Refactoring

--- Introduction/hello_world_part_one/hello_world_pt_one.py
+++ Introduction/hello_world_part_one/hello_world_pt_one.py
@@ -1,5 +1,10 @@
-print("Hello World!");
-x = "Hello Python";
-print(x);
-y = 42;
-print(y);
+# Console log in Python
+print("Hello World!")
+# Declaring a variable and Assigning a Value of a String to it in Python
+x = "Hello Python"
+# Console logging the Variable in Python
+print(x)
+# Declaring a Variable and Assigning a Value of a Number to it in Python
+y = 42
+# Console logging the Variable in Python
+print(y)
